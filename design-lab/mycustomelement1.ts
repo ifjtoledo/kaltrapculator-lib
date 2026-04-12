6730e972d74a35dd47ee177374cc81f0a2c8d90a
ifjtoledo <ifjtoledo@gmail.com>
2026-04-11 22:06:09 -0500

    feat: implement modal confirmation dialog in custom element

--- design-lab/mycustomelement1.ts
+++ design-lab/mycustomelement1.ts
@@ -4,3 +4,31 @@ interface ConfirmOptions {
     confirmText?: string;
     cancelText?: string;
 }
+interface ModaleteAPI {
+    confirmandias(optionmandias: ConfirmOptions): Promise<boolean>;
+    openmandias(): void;
+    closemandias(): void;
+}
+
+class ModaleteUi extends HTMLElement implements ModaleteAPI {
+    private shadowmandias: ShadowRoot;
+    private resolvermandias?: (value: boolean) => void;
+    constructor() {
+        super();
+        this.shadowmandias = this.attachShadow({ mode: 'open' });
+    }
+    connectedCallback(): void {
+        this.renderShellmandias();
+        this.bindEventsmandias();
+        this.bindKeyboardmandias();
+    }
+    confirmandias(optionmandias: ConfirmOptions): Promise<boolean> {
+        throw new Error('Method not implemented.');
+    }
+    openmandias(): void {
+        throw new Error('Method not implemented.');
+    }
+    closemandias(): void {
+        throw new Error('Method not implemented.');
+    }
+}
